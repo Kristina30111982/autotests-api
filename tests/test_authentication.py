8361a5e2185f190bb9311cec4fabcfbd167fa7ce
Kristina30111982 <159319046+Kristina30111982@users.noreply.github.com>
2026-03-16 15:57:05 +0300

    Написание API автотестов используя Pytest

--- tests/test_authentication.py
+++ tests/test_authentication.py
@@ -1,33 +1,32 @@
 from http import HTTPStatus
-from clients.authentication.authentication_schema import LoginResponseSchema
+
+from clients.authentication.authentication_schema import LoginResponseSchema, LoginRequestSchema
 from tools.assertions.authentication import assert_login_response
 from tools.assertions.base import assert_status_code
-from clients.users.public_users_client import get_public_users_client
-from clients.users.users_schema import CreateUserRequestSchema, CreateUserResponseSchema
+from clients.authentication.authentication_client import get_authentication_client
+from clients.public_http_builder import get_public_http_client
 
 
 def test_login():
-    """
-    Тест проверяет успешную аутентификацию пользователя.
-    """
-    # 1. Создаем нового пользователя
     public_users_client = get_public_users_client()
+    authentication_client = get_authentication_client()
 
-    request = CreateUserRequestSchema()
-    response = public_users_client.create_user_api(request)
+    # создаем пользователя
+    created_user = public_users_client.create_user()
 
-    # 2. Выполняем аутентификацию (передаем email и password из созданного юзера)
-    login_response = AuthenticationClient.login_api(
-        email=user_data.email,
-        password=user_data.password
+    # логинимся
+    login_response = authentication_client.login_api(
+        email=create_user_request.email,
+        password=create_user_request.password
     )
 
-    # 3. Проверяем статус-код (200 OK)
+    # проверка status code
     assert_status_code(login_response.status_code, HTTPStatus.OK)
 
-    # 4. Десериализуем и валидируем JSON-ответ по схеме
-    # model_validate_json автоматически проверит соответствие типов данных
-    login_response_data = LoginResponseSchema.model_validate_json(login_response.text)
+    # десериализация ответа
+    login_response_data = LoginResponseSchema.model_validate_json(
+        login_response.text
+    )
 
-    # 5. Проверяем корректность логических данных в теле ответа
+    # проверка тела ответа
     assert_login_response(login_response_data)
