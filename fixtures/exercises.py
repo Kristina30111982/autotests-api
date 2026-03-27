89518016ea3bdcb2a87609f532cbbc6650da2368
Kristina30111982 <159319046+Kristina30111982@users.noreply.github.com>
2026-03-27 14:02:34 +0300

    Практика реализации фикстур коррект

--- fixtures/exercises.py
+++ fixtures/exercises.py
@@ -2,14 +2,14 @@ import pytest
 from pydantic import BaseModel
 
 from clients.exercises.exercises_client import ExercisesClient, get_exercises_client
-from clients.exercises.courses_schema import CreateCourseRequestSchema, CreateCourseResponseSchema
-from fixtures.files import FileFixture
+from clients.exercises.exercises_schema import CreateExerciseRequestSchema, CreateExerciseResponseSchema
+from fixtures.courses import CourseFixture
 from fixtures.users import UserFixture
 
 
-class ExercisesFixture(BaseModel):
-    request: CreateCourseRequestSchema
-    response: CreateCourseResponseSchema
+class ExerciseFixture(BaseModel):
+    request: CreateExerciseRequestSchema
+    response: CreateExerciseResponseSchema
 
 
 @pytest.fixture
@@ -20,13 +20,8 @@ def exercises_client(function_user: UserFixture) -> ExercisesClient:
 @pytest.fixture
 def function_exercise(
         exercises_client: ExercisesClient,
-        function_user: UserFixture,
-        function_file: FileFixture,
         function_course: CourseFixture
-) -> ExercisesFixture:
-    request = CreateCourseRequestSchema(
-        preview_file_id=function_file.response.file.id,
-        created_by_user_id=function_user.response.user.id
-    )
+) -> ExerciseFixture:
+    request = CreateExerciseRequestSchema(course_id=function_course.response.course.id)
     response = exercises_client.create_exercise(request)
-    return ExercisesFixture(request=request, response=response)
+    return ExerciseFixture(request=request, response=response)
